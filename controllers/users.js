b53d4c15ce4a3e7ce57fee46d7ee2113ae574030
YehonatamBinyamini <yonile2106@gmail.com>
2023-05-29 11:45:51 +0300

    creating mongoose models for user + hashFunctions

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,35 @@
+const User = require("../models/User");
+
 exports.postLoginDetails = (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
+
+  // Perform your authentication logic here
+  // For demonstration purposes, we'll just return the received credentials
+  res.json({ email, password, server: "say hi" });
+};
+
+exports.postAddUser = async (req, res, next) => {
+    const { name, email, password } = req.body;
   
-    // Perform your authentication logic here
-    // For demonstration purposes, we'll just return the received credentials
-    res.json({ email, password, server: "say hi" });
-}
+    try {
+      const existingUser = await User.findOne({ email: email });
+      if (existingUser) {
+        return res.status(409).json({ error: "כבר  קיים משתמש עם דואר אלקטרוני זה" });
+      }
+  
+      const newUser = new User({
+        name: name,
+        email: email,
+        password: password,
+      });
+  
+      await newUser.save();
+      console.log("User created!");
+  
+      res.status(201).json({ message: "User created successfully." });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to create user." });
+    }
+  };
+  
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const usersController = require("../controllers/users");
 const router = express.Router();
 
 router.post("/login", usersController.postLoginDetails);
+router.post("/newUser", usersController.postAddUser);
 
 module.exports = router;
