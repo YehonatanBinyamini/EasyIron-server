6a1edc70b4b6c74d1b39325c4d091092f5e8638e
YehonatamBinyamini <yonile2106@gmail.com>
2023-05-29 17:13:00 +0300

    fixes with hashCode + password validation

--- controllers/users.js
+++ controllers/users.js
@@ -1,35 +1,59 @@
 const User = require("../models/User");
+const { generateHashCode, isPasswordFit } = require("../util/hashFunctions");
+const { tokenIsValid } = require("../util/validation");
 
-exports.postLoginDetails = (req, res, next) => {
+exports.postLoginDetails = async (req, res, next) => {
   const { email, password } = req.body;
-
+  let passwordIsFit = false;
+  try {
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      console.log(existingUser.hashCode);
+      isPasswordFit(existingUser.hashCode, password)
+        ? (passwordIsFit = true)
+        : (passwordIsFit = false);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to find user." });
+  } finally {
+    res.json({ passwordIsFit });
+  }
   // Perform your authentication logic here
   // For demonstration purposes, we'll just return the received credentials
-  res.json({ email, password, server: "say hi" });
+  //res.json({ passwordIsFit, message: "User logged in successfully." });
 };
 
 exports.postAddUser = async (req, res, next) => {
-    const { name, email, password } = req.body;
-  
-    try {
-      const existingUser = await User.findOne({ email: email });
-      if (existingUser) {
-        return res.status(409).json({ error: "כבר  קיים משתמש עם דואר אלקטרוני זה" });
-      }
-  
-      const newUser = new User({
-        name: name,
-        email: email,
-        password: password,
-      });
-  
-      await newUser.save();
-      console.log("User created!");
-  
-      res.status(201).json({ message: "User created successfully." });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Failed to create user." });
+  const { name, email, password } = req.body;
+  if (!tokenIsValid(password)) {
+    return res.status(500).json({ error: "סיסמה לא תקינה" });
+  }
+  try {
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ error: "כבר קיים משתמש עם דואר אלקטרוני זה" });
     }
-  };
-  
+    const hashCode = generateHashCode(password);
+
+    const newUser = new User({
+      name: name,
+      email: email,
+      hashCode: hashCode,
+    });
+
+    await newUser.save();
+    console.log("User created!");
+
+    return res
+      .status(201)
+      .json({ Login: true, message: "User created successfully." });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ Login: false, error: "Failed to create user." });
+  }
+};
